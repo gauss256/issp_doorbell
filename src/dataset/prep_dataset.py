e9fc8308c89af0b7248d5f5e8946f609eb438279
normanlim <norman.lim2@hotmail.com>
2016-05-19 17:10:39 -0700

    updated main for greyscale computations

--- src/dataset/prep_dataset.py
+++ src/dataset/prep_dataset.py
@@ -3,6 +3,7 @@ from __future__ import division
 
 import os
 import glob
+from matplotlib import pyplot as plt
 import pickle
 import random
 import numpy as np
@@ -24,9 +25,28 @@ WIDTH, HEIGHT = 352 // DOWNSAMPLE, 240 // DOWNSAMPLE
 IMAGE_ROOT = os.path.expanduser('~/Doorbell/images')
 
 def load_image(path):
-    img = imread(path)
+    img = imread(path, as_grey=True)
+    #img = imread(path, as_grey=False)
+        
+    # Normalize the image to have mean zero
+    # Stretch the intensities so 5 - 95 percentile goes to [-1, 1]
+    normalize = True
+    if normalize:
+        img = img.astype(float) / 255.0
+        img = img - np.average(img)
+        scale = np.max(np.abs(np.percentile(img, 5.0)), np.abs(np.percentile(img, 95.0)))
+        img = img / scale
+        img = np.clip(img, -1.0, 1.0)
+    
+        # Mask the timestamp
+        img[220:234, 96:258] = 0.0
+    
+    #plt.imshow(img, cmap='gray')  #!!!
+    #plt.show() #!!!
+
     if DOWNSAMPLE != 1:
         img = imresize(img, (HEIGHT, WIDTH))
+
     return img
 
 def load_train(base):
@@ -45,8 +65,12 @@ def load_train(base):
             img = load_image(path)
             if i == 0:
                 imsave('c{}.jpg'.format(j), img)
-            img = img.swapaxes(2, 0)
-
+           
+            if len(img.shape) == 3:
+                img = img.swapaxes(2, 0)
+            else:
+                img = img.swapaxes(1, 0)
+	
             X_train.append(img)
             y_train.append(j)
 
@@ -61,28 +85,40 @@ def load_train(base):
 
 def load_test(base):
     X_test = []
+    y_test = []
     X_test_id = []
-    paths = glob.glob(os.path.join(base, '*.jpg'))
-
-    if SUBSET:
-        paths = paths[:100]
 
     print('Reading test images...')
-    for i, path in tqdm(enumerate(paths), total=len(paths)):
-        img_id = os.path.basename(path)
-        img = load_image(path)
-        img = img.swapaxes(2, 0)
+    for j in range(NUM_CLASSES):
+        print('Loading folder c{}...'.format(j))
+        paths = glob.glob(os.path.join(base, 'c{}'.format(j), '*.jpg'))
 
-        X_test.append(img)
-        X_test_id.append(img_id)
+        if SUBSET:
+            paths = paths[:100]
+
+        for i, path in tqdm(enumerate(paths), total=len(paths)):
+            img_id = os.path.basename(path)
+            
+	    img = load_image(path)
+            if i == 0:
+                imsave('c{}.jpg'.format(j), img)
+            
+            if len(img.shape) == 3:
+                img = img.swapaxes(2, 0)
+            else:
+                img = img.swapaxes(1, 0)
+
+            X_test.append(img)
+            y_test.append(j)
+            X_test_id.append(img_id)
 
     X_test = np.array(X_test)
+    y_test = np.array(y_test)
     X_test_id = np.array(X_test_id)
-
-    return X_test, X_test_id
+    return X_test, y_test, X_test_id
 
 X_train, y_train = load_train(os.path.join(IMAGE_ROOT, 'train'))
-X_test, X_test_ids = load_test(os.path.join(IMAGE_ROOT, 'test'))
+X_test, y_test, X_test_ids = load_test(os.path.join(IMAGE_ROOT, 'test'))
 
 if SUBSET:
     dest = 'data_{}_subset.pkl'.format(DOWNSAMPLE)
@@ -90,4 +126,4 @@ else:
     dest = 'data_{}.pkl'.format(DOWNSAMPLE)
 
 with open(dest, 'wb') as f:
-    pickle.dump((X_train, y_train, X_test, X_test_ids), f)
+    pickle.dump((X_train, y_train, X_test, y_test, X_test_ids), f)
--- src/main.py
+++ src/main.py
@@ -5,15 +5,18 @@ import os
 import time
 import pickle
 import numpy as np
+import sys
 
+from PIL import Image
 from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 from keras.layers.core import Dense, Activation, Flatten, Dropout
-from keras.layers.convolutional import Convolution2D
+from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
+from keras.models import model_from_json
 
-from sklearn.metrics import log_loss
+from sklearn.metrics import log_loss, accuracy_score
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import KFold
 
@@ -34,18 +37,62 @@ mkdirp(CHECKPOINT_PATH)
 mkdirp(SUMMARY_PATH)
 mkdirp(MODEL_PATH)
 
-NB_EPOCHS = 5 if not TESTING else 1
-MAX_FOLDS = 3
+NB_EPOCHS = 10 if not TESTING else 1
+MAX_FOLDS = 3 if not TESTING else 2
 
-WIDTH, HEIGHT, NB_CHANNELS = 352 // DOWNSAMPLE, 240 // DOWNSAMPLE, 3
+WIDTH, HEIGHT, NB_CHANNELS = 352 // DOWNSAMPLE, 240 // DOWNSAMPLE, 1
 BATCH_SIZE = 16  # [50]
 
 with open(DATASET_PATH, 'rb') as f:
-    X_train_raw, y_train_raw, X_test, X_test_ids = pickle.load(f)
+    X_train_raw, y_train_raw, X_test, y_test, X_test_ids = pickle.load(f)    
+    if NB_CHANNELS == 1:
+        X_train_raw = X_train_raw.reshape(X_train_raw.shape[0], 1, WIDTH, HEIGHT)
+        X_test = X_test.reshape(X_test.shape[0], 1, WIDTH, HEIGHT) 
 
 predictions_total = [] # accumulated predictions from each fold
 scores_total = [] # accumulated scores from each fold
 num_folds = 0
+
+def filereader(fname):
+	x = np.array(Image.open(fname))
+	
+	if len(x.shape) == 2:
+		#add an additional colour dimension if the only dimensions are width and height
+		return preprocess( x.reshape((1, 1) + x.shape) )
+	if len(x.shape) == 3:
+		return preprocess( x.reshape((1, 0) + x.shape) )	
+	
+def preprocess(X):
+	#this preprocessor crops one pixel along each of the sides of the images
+	#return X[:, :, 1:-1, 1:-1] / 255.0	
+	return X/ 255.0	
+
+def myGenerator(y, chunk_size, batch_size, fnames):
+
+	#read and preprocess first file to figure out the image dimensions
+	
+	#sample_file = filereader('train/train' + str(0) + '.png')
+	#sample_file = np.array(Image.open('sample.jpg'))
+        sample_file = filereader('sample.jpg')
+	new_img_colours, new_img_rows, new_img_cols = sample_file.shape[1:]
+	
+	pool = Pool(processes=16)
+	
+	while 1:
+		for i in xrange(np.ceil(1.0 * len(fnames) / chunk_size).astype(int)):
+			this_chunk_size = len(fnames[i * chunk_size : (i+1) * chunk_size])
+			X = pool.map(filereader, [fnames[i * chunk_size + i2]  for i2 in xrange(this_chunk_size)])
+			X = np.array(X).reshape((-1, new_img_colours, new_img_rows, new_img_cols)) #.astype('float32')
+			#if 'test' in fnames[0]:
+			print(str(i))			
+						
+			for j in xrange(int(np.floor(this_chunk_size/batch_size))): 
+				#training set
+				if not y is None:	
+					yield X[j*batch_size:(j+1)*batch_size], y[i*chunk_size+j*batch_size:i*chunk_size+(j+1)*batch_size]
+				#test set
+				else:
+					yield X[j*batch_size:(j+1)*batch_size] 
 
 def vgg_bn():
     model = Sequential()
@@ -74,6 +121,35 @@ def vgg_bn():
     model.compile(Adam(lr=1e-3), loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
+def model_mnist():
+    nb_filters = 32
+    nb_conv = 3
+    nb_pool = 2
+    nb_classes = 2
+    
+    model = Sequential()
+
+    model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                            border_mode='valid',
+                            input_shape=(NB_CHANNELS, WIDTH, HEIGHT)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='adadelta',
+                metrics=['accuracy'])
+    return model
+
 for train_index, valid_index in KFold(len(X_train_raw), n_folds=MAX_FOLDS, shuffle=True, random_state=RANDOM_STATE):
     print('Fold {}/{}'.format(num_folds + 1, MAX_FOLDS))
 
@@ -100,17 +176,37 @@ for train_index, valid_index in KFold(len(X_train_raw), n_folds=MAX_FOLDS, shuff
     mkdirp(summary_path)
 
     callbacks = [
-        EarlyStopping(monitor='val_loss', patience=2, verbose=0, mode='auto'),
-        ModelCheckpoint(checkpoint_path, monitor='val_loss', verbose=0, save_best_only=True, mode='auto'),
-        TensorBoard(log_dir=summary_path, histogram_freq=0)
+        #EarlyStopping(monitor='val_loss', patience=2, verbose=0, mode='auto'),
+        #ModelCheckpoint(checkpoint_path, monitor='val_loss', verbose=0, save_best_only=True, mode='auto'),
+        #TensorBoard(log_dir=summary_path, histogram_freq=0)
     ]
     print('Starting model fit...')
-    model.fit(X_train, y_train, \
-            batch_size=BATCH_SIZE, nb_epoch=NB_EPOCHS, \
-            shuffle=True, \
-            verbose=1, \
-            validation_data=(X_valid, y_valid), \
-            callbacks=callbacks)
+    generator = False
+    if generator:
+        chunk_size = 15000
+        # assumption : fnames_train is a list of all training images
+        fnames_train = os.listdir('/home/issp/Doorbell/images/train/c0')
+        fnames_train = fnames_train + os.listdir('/home/issp/Doorbell/images/train/c1')
+        print (fnames_train)
+        model.fit_generator(
+            myGenerator(y_train, chunk_size, BATCH_SIZE, fnames_train), 
+            samples_per_epoch = y_train.shape[0], 
+            nb_epoch = NB_EPOCHS, 
+            verbose = 2, 
+            callbacks = [], 
+            validation_data = None, 
+            class_weight = None 
+        ) # max_q_size = 10
+    else:
+        model.fit(
+            X_train, y_train,
+            batch_size=BATCH_SIZE, nb_epoch=NB_EPOCHS,
+            shuffle=True,
+            verbose=1,
+            validation_data=(X_valid, y_valid),
+            callbacks=callbacks
+        )
+
     print('Finished model fit...')
 
     predictions_valid = model.predict(X_valid, batch_size=100, verbose=1)
@@ -126,6 +222,14 @@ for train_index, valid_index in KFold(len(X_train_raw), n_folds=MAX_FOLDS, shuff
 
 score_geom = calc_geom(scores_total, MAX_FOLDS)
 predictions_geom = calc_geom_arr(predictions_total, MAX_FOLDS)
+score_test = log_loss(y_test, predictions_geom)
+y_predict = np.array(predictions_geom)
+y_predict = np.argmax(y_predict, axis=1)
+acc_test = accuracy_score(y_test, y_predict)
+
+print()
+print('Overall score: {}'.format(score_test))
+print('Overall accuracy: {:.1f}%'.format(acc_test * 100.0))
 
 submission_path = os.path.join(SUMMARY_PATH, 'submission_{}_{:.2}.csv'.format(int(time.time()), score_geom))
 write_submission(predictions_geom, X_test_ids, submission_path)
